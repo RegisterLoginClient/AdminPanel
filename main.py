1539680bfeda0c9a6fc130d32148be2d61558c0f
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-03-31 19:51:05 +0300

    Created function for sending requests to server

--- main.py
+++ main.py
@@ -3,3 +3,24 @@ from tkinter import messagebox
 import jsonpickle
 import socket
 
+
+IP = '127.0.0.1'
+PORT = 4000
+
+
+def send_request(action, username=None, password=None):
+    request = {
+        "action": action,
+        "username": username,
+        "password": password
+    }
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((IP, PORT))
+    client.send(jsonpickle.encode(request).encode('utf-8'))
+
+    response_data = client.recv(4096).decode('utf-8')
+    response = jsonpickle.decode(response_data)
+    client.close()
+
+    return response
